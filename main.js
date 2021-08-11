c9edacd172123c909a59a19a8fb2e129b3117e3f
Jadson Goulart de Matos <jadson@pop-os.localdomain>
2021-08-10 22:23:29 -0300

    0.0.4

--- main.js
+++ main.js
@@ -1,61 +1,76 @@
 var cavas;
 var ctx;
 var background;
+var backgroundCTX;
 var qrElement;
-var result;
+var result = [];
 var index;
+var min = -1;
+var value = 0;
+var decoder = new QCodeDecoder();
+var final = -1;
+var tamanhoBusca = 2048;
 
-window.onload = () => {
-  cavas = new OffscreenCanvas(85, 108);
+async function Start(input) {
+  tamanhoBusca = input
   background = document.querySelector("[data-image='background']");
   qrElement = document.querySelector("[data-image='qr']");
+  cavas = new OffscreenCanvas(108, 108);
   ctx = cavas.getContext("2d");
+  ctx.clearRect(0, 0, 108, 108);
+  ctx.drawImage(background, 0, 0);
+  backgroundCTX = ctx.getImageData(0, 0, 108, 108).data;
 
-  search();
+  await search();
+  console.log("Final");
   draw();
   drawQR();
-};
+}
 
 function draw() {
   //ctx.globalCompositeOperation = "source-over";
+  ctx.clearRect(0, 0, 108, 108);
   ctx.drawImage(background, 0, 0);
 
   qr = new QRious({
-    value: "https://webxr.run/XVPRm7NAGPARr#" + index,
+    value: "https://webxr.run/XVPRm7NAGPARr#" + value,
     backgroundAlpha: 0,
     level: "H",
   });
 
   //console.log(qr.toDataURL());
-  ctx.drawImage(qr.image, 8, 18, 72, 72);
+  ctx.drawImage(qr.image, 6, 15, 76, 76);
 
   const ctxCanvas = document.querySelector("canvas").getContext("2d");
 
-  const blob = ctx.getImageData(0, 0, 85, 108);
+  const blob = ctx.getImageData(0, 0, 108, 108);
   ctxCanvas.putImageData(blob, 0, 0);
 }
 
 function drawQR() {
   qr = new QRious({
     element: qrElement,
-    value: "https://webxr.run/XVPRm7NAGPARr#" + index,
-    backgroundAlpha: 0,
+    value: "https://webxr.run/XVPRm7NAGPARr#" + value,
+    backgroundAlpha: 1,
     level: "H",
   });
 }
 
 async function search() {
-  for (index = 0; index <= 1024; index++) {
-    result = await render();
-    console.log("index:", index, "result:", result);
-    if (result) {
-      break;
+  for (index = 0; index <= tamanhoBusca; index++) {
+    result.push(await render());
+
+    let newMin = Math.min(...result);
+    if (min > newMin || min == -1) {
+      min = newMin;
+      value = result.indexOf(newMin);
+      console.log("index:", index, "min:", min, "value:", value);
     }
   }
 }
 
 async function render() {
-  ctx.clearRect(8, 18, 72, 72);
+  ctx.clearRect(0, 0, 108, 108);
   ctx.drawImage(background, 0, 0);
 
   qr = await new QRious({
@@ -65,16 +80,27 @@ async function render() {
   });
 
   let blob = await qr.image;
-  ctx.drawImage(blob, 8, 18, 72, 72);
+  ctx.drawImage(blob, 6, 15, 76, 76);
+  let qrCTX = ctx.getImageData(0, 0, 108, 108).data;
 
-  let dif = false;
+  let dif = 0;
+
+  for (let index = 0; index < backgroundCTX.length; index++) {
+    dif = dif + (backgroundCTX[index] - qrCTX[index]);
+  }
 
   blob = await cavas.convertToBlob();
   let a = new FileReader();
   a.readAsDataURL(blob);
   a.onload = function (e) {
-    QCodeDecoder().decodeFromImage(e.target.result, (er, res) => {
-      dif = res;
+    let image = document.createElement("img");
+    image.src = e.target.result;
+    decoder.decodeFromImage(image, (err, res) => {
+      if (res) {
+        console.log("Resposta: ", res);
+        final = res;
+        index = tamanhoBusca;
+      }
     });
   };
 
